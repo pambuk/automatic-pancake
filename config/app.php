c815570b45844a94f9e4ec4ea129bbbd045a3f6e
pambuk <wojtek.zymonik@gmail.com>
2016-12-03 13:00:38 +0100

    debugbar

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         \Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        \Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
